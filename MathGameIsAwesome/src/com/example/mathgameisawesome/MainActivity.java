0c6e9b4a7d0c4d7fdbf972ea394e44021afcb7e3
Plastikrokk <fdbingham@gmail.com>
2014-04-23 00:56:59 -0400

    HUGE FIX
    Okay guys, I basically just sat down and cranked this out in one
    sitting.  So far, the first button is the ONLY right answer.  I need to
    figure out how to randomly assign a button the be the correct answer
    while showing the correct answer.  After that, it's just polishing the
    look up.
    
    Also it only does addition right now.

--- MathGameIsAwesome/src/com/example/mathgameisawesome/MainActivity.java
+++ MathGameIsAwesome/src/com/example/mathgameisawesome/MainActivity.java
@@ -1,68 +1,160 @@
 package com.example.mathgameisawesome;
 
-/*
- * 
- */
-
-//import android.support.v7.app.ActionBarActivity;
-//import android.support.v7.app.ActionBar;
-//import android.support.v4.app.Fragment;
-import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.os.Build;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
-	private TextView inputView = null;
-	
-	//int num = RandRange(10, 1);
-	/*
-	 * HEY GUYS LOOK HERE So I had an idea.  If we simply use the random number generator method to generate 8 numbers in sets of 2, we can use the 
-	 * 	resulting 4 answers as potential answers to the true questions, which is one of these sets.  This way we don't have to 
-	 * 	deal with a number pad and instead can have only 4 clickable areas.  Thoughts?
-	 */
-	
-	
-	@Override
+	private Button okButton;
+	private Button answer1;
+	private Button answer2;
+	private Button answer3;
+
+	@Override	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		//
-		inputView = (TextView) findViewById(R.id.inputView);
-		int x = getRandomNumber(10, 1);
-		int y = getRandomNumber(10, 1);
-		int compVal = x + y;
-		inputView.setText(""+ compVal);
+		setContentView(R.layout.fragment_main);
 		
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		int x = getRandomNumber(10,1);
+		while (x == 0)
+			x = getRandomNumber(10,1);
+		int y = getRandomNumber(10,1);
+		while (y == 0)
+			y = getRandomNumber(10,1);
+		
+		AlertDialog alert1 = new AlertDialog.Builder(this).create();
+		alert1.setMessage("You win!");
+		
+		
+		
+		TextView textView = (TextView) findViewById(R.id.textView);
+		textView.setText(""+x);
+		textView.setTextColor(Color.GREEN);
+		textView.setBackgroundColor(Color.RED);
+		TextView text2 = (TextView) findViewById(R.id.text1);
+		text2.setText(""+y);
+		text2.setBackgroundColor(Color.GREEN);
+		text2.setTextColor(Color.BLACK);
+		TextView oper = (TextView) findViewById(R.id.operator);
+		oper.setText(displayOperator(0));
+		
+		this.answer1=(Button)this.findViewById(R.id.answer1);
+		answer1.setText(""+(x+y));
+		this.answer1.setOnClickListener(new OnClickListener() {
+			@SuppressWarnings("deprecation")
+			public void onClick(View v) {
+				AlertDialog alert1 = new AlertDialog.Builder(MainActivity.this).create();
+				alert1.setMessage("You win!");
+				alert1.setButton("OK!", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+						Toast.makeText(getApplicationContext(), "You win!  Yay!", Toast.LENGTH_SHORT).show();
+					}
+				});
+				
+				alert1.show();
+			}
+		});
+		
+		
+		this.answer2=(Button)this.findViewById(R.id.answer2); 
+		int w1 = getRandomNumber(10,1);
+		int w2 = getRandomNumber(10,1);
+		while ((w1+w2) == (x+y))
+		{
+			w1 = getRandomNumber(10,1);
+			w2 = getRandomNumber(10,1);
+		}
+		answer2.setText(""+(w1+w2));
+		this.answer2.setOnClickListener(new OnClickListener() {
+			@SuppressWarnings("deprecation")
+			public void onClick(View v) {
+				AlertDialog alert2 = new AlertDialog.Builder(MainActivity.this).create();
+				alert2.setMessage("Wrong Answer!");
+				alert2.setButton("OK!", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+						Toast.makeText(getApplicationContext(), "Try again?", Toast.LENGTH_SHORT).show();
+					}
+				});
+				alert2.show();
+			}
+		});
+		
+		
+		this.answer3=(Button)this.findViewById(R.id.answer3);
+		int w3 = getRandomNumber(10,1);
+		int w4 = getRandomNumber(10,1);
+		while ((w3+w4)==(x+y) && (w3+w4) == (w1+w2))
+		{
+			w3 = getRandomNumber(10,1);
+			w4 = getRandomNumber(10,1);
+		}
+		answer3.setText(""+(w3+w4));
+		this.answer3.setOnClickListener(new OnClickListener() {
+			@SuppressWarnings("deprecation")
+			public void onClick(View v) {
+				AlertDialog alert3 = new AlertDialog.Builder(MainActivity.this).create();
+				alert3.setMessage("Wrong answer!");
+				alert3.setButton("OK!", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+						Toast.makeText(getApplicationContext(), "Try again?", Toast.LENGTH_SHORT).show();
+					}
+				});
+				alert3.show();
+			}
+		});
+		
+		
+		
+		this.okButton=(Button)this.findViewById(R.id.okbutton);
+		this.okButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				finish();
+			}
+		});
+		
+	
 	}
 	
-	public int getRandomNumber(int max, int min) 
+	public int getRandomNumber(int max, int min)
 	{
-		int range = (max - min) + 1;
+		int range = (max-min) + 1;
 		return (int)(Math.random() * range);
 	}
 	
-	public void numberCrunch()
+	public String displayOperator (int num)
 	{
-		int x = getRandomNumber(10, 1);
-		int y = getRandomNumber(10, 1);
-		int compVal = x + y;
-		
-		
-		
+		if (num == 0)
+		{
+			return "+";
+		}
+		else if (num == 1)
+		{
+			return "-";
+		}
+		else if (num == 2)
+		{
+			return "*";
+		}
+		else
+		{
+			return "/";
+		}
 	}
+	
 }
